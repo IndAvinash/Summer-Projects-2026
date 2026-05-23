9b9730d594f222968b46441a853e49a0ed3124df
Avinash Kumar <avinashku200612@gmail.com>
2026-05-23 05:40:17 +0530

    implemented redis caching of users

--- PrivaSync/250242_Avinash_Kumar/week-1/main.py
+++ PrivaSync/250242_Avinash_Kumar/week-1/main.py
@@ -1,48 +1,96 @@
 from fastapi import Depends, FastAPI, HTTPException, Query
 from typing import Generator, Optional, List
 from contextlib import asynccontextmanager
+import json
 from sqlmodel import Field, SQLModel, create_engine, Session, select
-from pydantic import BaseModel
+import redis.asyncio
 
-class User(SQLModel,table=True):
-    id: Optional[int] = Field(primary_key=True,default=None)
-    name:str
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+
+
 sqlurl = "sqlite:///data.db"
-engine = create_engine(sqlurl,echo=True)
+engine = create_engine(sqlurl, echo=True)
 
 
-app = FastAPI()
-@app.on_event("startup")
-async def on_startup():
+@asynccontextmanager
+async def lifecycle(app: FastAPI):
+    global redis_client
     SQLModel.metadata.create_all(engine)
+    redis_client = redis.asyncio.Redis(decode_responses=True) # port:6379
+    await redis_client.ping()
+    yield
+    await redis_client.close()
+
+app = FastAPI(lifespan=lifecycle)
+
+
 @app.get("/")
 async def home():
     return {"data": "Hello World"}
 
-def get_session() -> Generator[Session, None, None]:
+
+async def get_session():
     with Session(engine) as session:
         yield session
+async def get_redis():
+    return redis_client
 
 @app.get("/users", response_model=List[User])
+
 def read_users(
     session: Session = Depends(get_session),
     offset: int = 0,
-    limit: int = Query(default=100, le=100) # Limits max results to 100 per request
+    limit: int = Query(default=100, le=100)
 ):
     statement = select(User).offset(offset).limit(limit)
-    users = session.exec(statement)
+    users = session.exec(statement).all()
+
     return users
 
-@app.post("/add")
-def create_hero(user: User):
+@app.get("/user/{id}")
+async def get_user(session: Session = Depends(get_session), id: int = None,redis:redis.asyncio.Redis =Depends(get_redis)):
+    # user = await 
+    key = f"user:{id}"
+    user =await redis.get(key)
+    if(user):
+        print("returning from cache")
+
+        return {
+            "source":"cache",
+            "user": json.loads(user),
+        }
+    
+    user = session.get(User, id)
+    
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    data = user.model_dump()
+    await redis.set(key,json.dumps(data),ex=10)
+    print("returning from cache")
+    return {
+            "source":"databases",
+            "user":user,
+        }
+
+@app.post("/add", response_model=User)
+def create_user(
+    user: User,
+    session: Session = Depends(get_session)
+):
     try:
-        with Session(engine) as session:
-            session.add(user)
-            session.commit()
-            session.refresh(user)
-            return user
-    except:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+        return user
+
+    except Exception as e:
         raise HTTPException(
-            status_code=404,
-            detail={'error': 'User can\'t be inserted', 'user_id': user_id} #This is a dict for customizing the error message
+            status_code=500,
+            detail={
+                "error": "User can't be inserted",
+                "message": str(e)
+            }
         )
